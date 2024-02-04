693583e125a20bc348de7187542fecfa36f01326
sandeepbilthare <sandeep.bilthare@gmail.com>
2024-02-04 19:40:15 +0530

    First commit with first set of results

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+
+# load libraries
+library(readr)
+library(dplyr)
+
+# Read feature names and assign those to column names of X_test
+features <- read_delim("./data/features.txt", col_names = c("id", "feature_name"),delim = " ")
+
+# Read test datasets
+subject_test <- read_csv("./data/test/subject_test.txt", col_names = "subject")
+X_test <- read.table("./data/test/X_test.txt", sep = "" , header = F,
+                   na.strings ="", stringsAsFactors= F)
+
+y_test <- read_csv("./data/test/y_test.txt", col_names = "outcome") 
+
+# Sssign feature names to column names of X_test
+colnames(X_test) <- features$feature_name
+
+test <- cbind(subject_test, X_test, y_test)
+# test <- data.frame(cbind(c(subject_test, X_test, y_test)))
+
+# Read train datasets
+subject_train <- read_csv("./data/train/subject_train.txt", col_names = "subject") 
+X_train <- read.table("./data/train/X_train.txt", sep = "" , header = F,
+                     na.strings ="", stringsAsFactors= F)
+
+y_train <- read_csv("./data/train/y_train.txt", col_names = "outcome")
+colnames(X_train) <- features$feature_name
+
+
+train <- cbind(subject_train, X_train, y_train)
+
+
+sum(colnames(test) == colnames(train))
+
+# Join the datasets
+myData <- rbind(test, train)
+
+
+# Extract only the measurements on the mean and standard deviation for each measurement
+stdmeancols <- which(grepl("(std|mean)[(][)](-[XYZ])?$",colnames(myData)))
+
+filteredData <- myData %>% select(subject, stdmeancols)
+
+# Create a new dataset with mean of activity for each subject
+avgValues <- filteredData %>% 
+  group_by(subject) %>% 
+  summarise_all(mean, na.rm=TRUE)
+
+# Write avgValues calculated in previous step to a csv file
+write.table(avgValues, file = "./output/avgValues.txt", row.names = FALSE)
